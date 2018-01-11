e344c18091e3bff22f60f7c2daf551b47a7b0e59
Robert Laverty <roblav96@gmx.com>
2018-01-10 19:02:06 -0500

    simulate backend, then saving

--- src/app/app.js
+++ src/app/app.js
@@ -7,13 +7,15 @@ import Vue from 'vue'
 export default {
 
 	mounted() {
-		(this.$refs.search_input).focus()
+		this.$refs.search_input.focus()
 	},
 
 	data() {
 		return {
 			drawer: false,
 			editingId: -1,
+			editing: {},
+			saving: false,
 			search: '',
 			headers: [
 				{ text: 'Name', value: 'name', align: 'left' },
@@ -38,19 +40,6 @@ export default {
 			})
 		},
 
-		editing() {
-			return this.$store.state.accessLevels.find(v => v.id == this.editingId) || {}
-			// let accessLevel = this.$store.state.accessLevels.find(v => v.id == this.editingId)
-			// let reader = this.$store.state.readers.find(v => v.id == accessLevel.readerId) || {}
-			// let type = this.$store.state.readerTypes.find(v => v.id == reader.typeId) || {}
-			// return {
-			// 	id: accessLevel.id,
-			// 	name: accessLevel.name,
-			// 	reader: reader.name,
-			// 	type: type.name,
-			// }
-		},
-
 	},
 
 	methods: {
@@ -58,12 +47,38 @@ export default {
 		setEditing(item) {
 			this.drawer = true
 			this.editingId = item.id
+			this.editing = Object.assign({}, item)
+			this.$refs.editing_name.focus()
 		},
+
+		saveChanges() {
+			this.saving = true
+
+			// simulate sending changes to backend
+			setTimeout(editing => {
+				let accessLevel = this.$store.state.accessLevels.find(v => v.id == editing.id) || {}
+				Object.assign(accessLevel, editing)
+
+				let reader = this.$store.state.readers.find(v => v.id == accessLevel.readerId) || {}
+				let type = this.$store.state.readerTypes.find(v => v.id == reader.typeId) || {}
+
+				this.saving = false
+				this.drawer = false
+
+			}, 1000, this.editing)
+		}
 
 	},
 
 
 
 }
+
+
+
+
+
+
+
 
 
