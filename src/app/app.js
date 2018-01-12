6188cdc9ba26cf5e997406ce276302376050dd20
Robert Laverty <roblav96@gmx.com>
2018-01-11 23:52:39 -0500

    added reader selection

--- src/app/app.js
+++ src/app/app.js
@@ -19,6 +19,7 @@ export default {
 			search: '',
 			headers: [
 				{ text: 'Name', value: 'name', align: 'left' },
+				{ text: 'Description', value: 'description', align: 'left' },
 				{ text: 'Type', value: 'type', align: 'left' },
 				{ text: 'Reader', value: 'reader', align: 'left' },
 			],
@@ -34,10 +35,15 @@ export default {
 				return {
 					id: accessLevel.id,
 					name: accessLevel.name,
+					description: accessLevel.description,
 					reader: reader.name,
 					type: type.name,
 				}
 			})
+		},
+
+		readers() {
+			return this.$store.state.readers.map(v => v.name)
 		},
 
 	},
@@ -57,23 +63,23 @@ export default {
 			// simulate sending changes to backend
 			setTimeout(editing => {
 				let accessLevel = this.$store.state.accessLevels.find(v => v.id == editing.id) || {}
-				Object.assign(accessLevel, editing)
-
-				let reader = this.$store.state.readers.find(v => v.id == accessLevel.readerId) || {}
-				let type = this.$store.state.readerTypes.find(v => v.id == reader.typeId) || {}
+				accessLevel.name = editing.name
+				accessLevel.description = editing.description
+				
+				let reader = this.$store.state.readers.find(v => v.name == editing.reader) || {}
+				accessLevel.readerId = reader.id
 
 				this.saving = false
 				this.drawer = false
 
 			}, 1000, this.editing)
-		}
+		},
 
 	},
 
 
 
 }
-
 
 
 
